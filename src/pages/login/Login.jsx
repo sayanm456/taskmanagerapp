d8da34c7ff0e236d2dc94fe8a6e92924bc939489
Sayan <sayanm2345@gmail.com>
2025-01-02 01:05:32 +0530

    #

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
-import AuthContext from '../contexts/auth/AuthContext';
+import AuthContext from '../../contexts/auth/AuthContext';
 
 
 const Login = () => {
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import AuthContext from '../contexts/auth/AuthContext'
+import AuthContext from '../../contexts/auth/AuthContext'
 
 const Signup = () => {
 
