86ba29306acf083efc38712f458854dd29a2a171
Sayan <sayanm2345@gmail.com>
2025-01-02 02:00:38 +0530

    User authentication protectection in dashboard page to prevent unauthorized access in frontend ui

--- src/pages/dashboard/AdminDash.jsx
+++ src/pages/dashboard/AdminDash.jsx
@@ -1,8 +1,17 @@
-import React, { } from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../components/ui/Header'
 import TaskPage from '../../components/task/TaskPage'
+import { useNavigate } from 'react-router-dom'
 
 const AdminDash = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!localStorage.getItem('authtoken')){
+       navigate('/login')
+    }
+  }, [])
 
   return (
     <>
