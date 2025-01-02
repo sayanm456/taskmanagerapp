fbf8689af871c6a42010414f3c9505242c77a7d8
Sayan <sayanm2345@gmail.com>
2025-01-02 23:47:32 +0530

    #

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import { Outlet } from 'react-router-dom'
-import Header from './ui/Header'
+import Header from './Header'
 
 const Layout = () => {
   return (
